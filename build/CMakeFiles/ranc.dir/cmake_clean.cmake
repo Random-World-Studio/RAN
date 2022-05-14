1f43b656fb8e54c4f94b34459d4ea15d844b2148
pointer-to-bios <yyb1738487169@163.com>
2022-05-14 10:02:28 +0800

    update some

---
+++ build/CMakeFiles/ranc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ranc.dir/src/compiler/main.cc.o"
+  "CMakeFiles/ranc.dir/src/compiler/main.cc.o.d"
+  "ranc"
+  "ranc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ranc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
